4dd34008491cc0c1620211cb25ccac6a8ebc4c0d
Sankrita Patel <sankrita6960@outlook.com>
2024-05-17 15:44:19 +0530

    Utils added

--- template.py
+++ template.py
@@ -24,8 +24,6 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
-    f'src/{project_name}/exception.py',
-    f'src/{project_name}/logger.py',
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
