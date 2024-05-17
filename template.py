d9c33df4d8a74d06c1018f6b9fdca3e71ea2ee1d
Sankrita Patel <sankrita6960@outlook.com>
2024-05-17 13:42:49 +0530

    Folder structure created

---
+++ template.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+
+project_name = "redWineQuality"
+
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f'src/{project_name}/components/data_ingestion.py',
+    f'src/{project_name}/components/data_transformation.py',
+    f'src/{project_name}/components/model_trainer.py',
+    f'src/{project_name}/components/model_monitoring.py',
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f'src/{project_name}/pipeline/training_pipeline.py',
+    f'src/{project_name}/pipeline/prediciton_pipeline.py',
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    f'src/{project_name}/exception.py',
+    f'src/{project_name}/logger.py',
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating Directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass #Create an empty file
+        logging.info(f"Creating empty files: {filepath}")
+    else:
+        logging.info(f"{filename} is already exists.")
