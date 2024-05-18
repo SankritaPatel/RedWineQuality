064c455d354dcf38761576ffcb73f77f42eb94a3
Sankrita Patel <sankrita6960@outlook.com>
2024-05-18 10:47:14 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.redWineQuality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.redWineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.redWineQuality import logging
 from src.redWineQuality.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
@@ -17,6 +18,16 @@ STAGE_NAME = "Data Validation stage"
 try:
    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
