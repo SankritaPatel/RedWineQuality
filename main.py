ce2dd6c57ffe1968fac3cc4710ff15aedcf6d5a9
Sankrita Patel <sankrita6960@outlook.com>
2024-05-18 11:03:59 +0530

    Model Trainer Done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.redWineQuality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from src.redWineQuality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.redWineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.redWineQuality import logging
@@ -28,6 +29,17 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
